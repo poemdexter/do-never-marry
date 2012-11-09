2fce35be904d496ebc58da372dd39436f233ce47
Daniel Fairley <dfairley@credera.com>
2012-11-09 12:32:38 -0600

    preparing for heroku

--- Rakefile
+++ Rakefile
@@ -1,2 +1,2 @@
-require './app'
+require './web'
 require 'sinatra/activerecord/rake'
--- web.rb
+++ web.rb
@@ -1,6 +1,7 @@
 require 'sinatra'
 require 'sinatra/activerecord'
 require './bin/reason'
+require 'uri'
 
 configure :development, :test do
   set :database, 'sqlite3:///dnm.db'
